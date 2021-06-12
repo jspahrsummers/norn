49903df169856e222e2f6590a99af98f741e5e52
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-06-12 18:49:31 +0100

    Start a test suite

---
+++ spec/gamechain/hash_spec.lua
@@ -0,0 +1,18 @@
+require("busted.runner")
+
+local hash = require("gamechain.hash")
+
+describe("hash", function ()
+	it("should produce a 32-byte hash", function ()
+		local h = hash("foobar")
+		assert.are.same(#h, 32)
+	end)
+
+	it("consistently hashes the same value", function ()
+		assert.are.same(hash("foobar"), hash("foobar"))
+	end)
+
+	it("should hash different data differently", function ()
+		assert.are_not.same(hash("foobar"), hash("fuzzbuzz"))
+	end)
+end)
