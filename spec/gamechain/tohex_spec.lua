043d3cdfad6b82c2c551a0c855241b3b5bc1e4b8
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-06-12 18:57:31 +0100

    Tests for tohex

---
+++ spec/gamechain/tohex_spec.lua
@@ -0,0 +1,17 @@
+require("busted.runner")
+
+local tohex = require("gamechain.tohex")
+
+describe("tohex", function ()
+	it("should do nothing for empty string", function ()
+		assert.are.same(tohex(""), "")
+	end)
+
+	it("should return hex for one byte", function ()
+		assert.are.same(tohex("a"), "61")
+	end)
+
+	it("should return hex for multiple bytes", function ()
+		assert.are.same(tohex("Aa?"), "41613f")
+	end)
+end)
