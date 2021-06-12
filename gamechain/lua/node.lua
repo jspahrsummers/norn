7d244caa965c2bb309f3fb9659e36e896ec992d1
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-06-12 17:12:22 +0100

    Some networking-ish code

--- gamechain/lua/node.lua
+++ gamechain/lua/node.lua
@@ -11,8 +11,24 @@ setmetatable(Node, {
 	end,
 })
 
-function Node:init(wallet_privkey)
+function Node:init(networker, peer_list, wallet_privkey)
+	assert(networker, "Node must be created with a networker to use")
+
+	self.networker = networker
+	self.peer_set = peer_list_to_set(peer_list or {})
 	self.wallet_privkey = wallet_privkey
+end
+
+--- Runs the node logic forever, or until an error is raised.
+-- This function never returns normally.
+function Node:run()
+	while true do
+		local sender, bytes = self.networker:recv()
+		self.peer_set[sender] = true
+
+		local msg = message.decode(bytes)
+		self:handle_message(sender, msg)
+	end
 end
 
 function Node:create_wallet()
@@ -23,47 +39,64 @@ function Node:create_wallet()
 	-- TODO
 end
 
-function Node:handle_message(msg)
+function Node:handle_message(sender, msg)
 	local handlers = {
 		[M.APP_DEFINED] = self.app_defined,
 		[M.PING] = self.ping,
 		[M.PONG] = self.pong,
 		[M.REQUEST_PEER_LIST] = self.request_peer_list,
 		[M.PEER_LIST] = self.peer_list,
-		[M.WALLET_BALANCE] = self.wallet_balance,
 	}
 
 	local name = msg[1]
 	local handler = handlers[name]
 	if handler then
-		handler(self, table.unpack(msg, 2))
+		handler(self, sender, table.unpack(msg, 2))
 	else
 		io.stderr:write("Non-producer node cannot handle message ", name)
 	end
 end
 
-function Node:app_defined(...)
+function Node:app_defined(sender, ...)
 	-- TODO
 end
 
-function Node:ping(token)
+function Node:ping(sender, token)
+	local msg = message.pong(token)
+	self.networker:send(sender, message.encode(msg))
+end
+
+function Node:pong(sender, token)
 	-- TODO
 end
 
-function Node:pong(token)
-	-- TODO
+function Node:request_peer_list(sender, token)
+	local msg = message.peer_list(peer_set_to_list(self.peer_set), token)
+	self.networker:send(sender, message.encode(msg))
 end
 
-function Node:request_peer_list(token)
-	-- TODO
+function Node:peer_list(sender, peers, maybe_token)
+	for _, peer in ipairs(peers) do
+		self.peer_set[peer] = true
+	end
 end
 
-function Node:peer_list(peers, maybe_token)
-	-- TODO
+local function peer_list_to_set(peer_list)
+	local set = []
+	for _, peer in ipairs(peer_list) do
+		set[peer] = true
+	end
+
+	return set
 end
 
-function Node:wallet_balance(token, wallet_pubkey, balance)
-	-- TODO
+local function peer_set_to_list(peer_set)
+	local list = []
+	for peer, _ in pairs(peer_set) do
+		list[] = peer
+	end
+
+	return list
 end
 
 return Node
