2d3dc728531232c4ac69d1a1d21c709271cbd45e
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-06-27 13:26:38 +0100

    Add convenience function for logging tables

--- norn/lua/logging.lua
+++ norn/lua/logging.lua
@@ -24,4 +24,50 @@ function M.debug(fmt, ...)
 	end
 end
 
+local LoggableTable = {}
+LoggableTable.__index = LoggableTable
+
+function M.explode_table(tbl)
+	if type(tbl) ~= "table" then
+		return tbl
+	end
+
+	local meta = getmetatable(tbl)
+	if meta and meta.__tostring then
+		return tbl
+	end
+
+	local tbl_copy = setmetatable({}, LoggableTable)
+	for k, v in pairs(tbl) do
+		tbl_copy[M.explode_table(k)] = M.explode_table(v)
+	end
+
+	return tbl_copy
+end
+
+local function quoted_tostring(v)
+	if type(v) == "string" then
+		return string.format("%q", v)
+	else
+		return tostring(v)
+	end
+end
+
+local function indent(s)
+	return string.gsub(s, "\n", "\n\t")
+end
+
+function LoggableTable:__tostring()
+	if not next(self) then
+		return "{}"
+	end
+
+	local s = "{"
+	for k, v in pairs(self) do
+		s = s .. string.format("\n\t[%s] = %s", indent(quoted_tostring(k)), indent(quoted_tostring(v)))
+	end
+
+	return s .. "\n}"
+end
+
 return M
---
+++ spec/norn/logging_spec.lua
@@ -0,0 +1,84 @@
+require("busted.runner")()
+
+local logging = require("norn.logging")
+
+describe("logging", function ()
+	describe("explode_table", function ()
+		it("should stringify empty table", function ()
+			local tbl = {}
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, "{}")
+		end)
+
+		it("should stringify a sequence", function ()
+			local tbl = { 2, 4, 6 }
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, [[{
+	[1] = 2
+	[2] = 4
+	[3] = 6
+}]])
+		end)
+
+		it("should quote string keys", function ()
+			local tbl = { foo = 5, bar = 10 }
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, [[{
+	["foo"] = 5
+	["bar"] = 10
+}]])
+		end)
+
+		it("should quote string values", function ()
+			local tbl = { "foo", "bar" }
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, [[{
+	[1] = "foo"
+	[2] = "bar"
+}]])
+		end)
+
+		it("should escape strings", function ()
+			local tbl = { ["foo'bar"] = 'fuzz"buzz' }
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, [[{
+	["foo'bar"] = "fuzz\"buzz"
+}]])
+		end)
+
+		it("should explode nested tables", function ()
+			local tbl = {
+				[{ fuzz = "buzz" }] = { "foo", "bar" }
+			}
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, [[{
+	[{
+		["fuzz"] = "buzz"
+	}] = {
+		[1] = "foo"
+		[2] = "bar"
+	}
+}]])
+		end)
+
+		it("should call custom __tostring", function ()
+			local a = setmetatable({}, {
+				__tostring = function (...)
+					return "a"
+				end
+			})
+
+			local b = setmetatable({}, {
+				__tostring = function (...)
+					return "b"
+				end
+			})
+
+			local tbl = { [a] = b }
+			local s = tostring(logging.explode_table(tbl))
+			assert.equals(s, [[{
+	[a] = b
+}]])
+		end)
+	end)
+end)
