8d89ad87202edce39e0efcc519e729493e669e51
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-06-28 21:58:04 +0100

    Meh, just ditch coroutine wrapper for demo
    Unnecessary complexity. Individual coroutines can be created as needed.

--- norn-demo/lua/main.lua
+++ norn-demo/lua/main.lua
@@ -4,26 +4,12 @@ local Wallet = require("norn.wallet")
 
 local DemoNetworker = require("norn.demo.networker")
 
-local main = coroutine.create(function ()
-	logging.level = logging.LOG_LEVEL_DEBUG
+logging.level = logging.LOG_LEVEL_DEBUG
 
-	local a = DemoNetworker("a")
-	local b = DemoNetworker("b")
+local a = DemoNetworker("a")
+local b = DemoNetworker("b")
 
-	a:send(b.address, 5)
-	local sender, bytes = b:recv()
-	logging.debug("sender: %s", logging.explode(sender))
-	logging.debug("bytes: %s", bytes)
-	return 0
-end)
-
-local success, result
-while coroutine.status(main) ~= "dead" do
-	success, result = coroutine.resume(main)
-	if not success then
-		logging.error(debug.traceback(main, result))
-		return 1
-	end
-end
-
-return result
+a:send(b.address, 5)
+local sender, bytes = b:recv()
+logging.debug("sender: %s", logging.explode(sender))
+logging.debug("bytes: %s", bytes)
